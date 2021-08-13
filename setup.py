9ae6bdc3d54153e72a216ce13dfa849dbc35ecd9
Jordan Schneider <jordan.jack.schneider@gmail.com>
2021-08-13 14:11:46 -0700

    Exposes (limited) typing information.

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
-from setuptools import find_packages, setup
+from setuptools import find_packages, setup  # type: ignore
+
+packages = find_packages()
+package_data = {package: ["py.typed"] for package in packages}
 
 setup(
-    name="phasic-policy-gradient", packages=find_packages(), version="0.0.1",
+    name="phasic-policy-gradient",
+    packages=packages,
+    version="0.0.1",
+    package_data=package_data,
 )
